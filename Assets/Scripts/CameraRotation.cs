4a30e13b4a833ba91bb1dc401f3faff7dbc21aa0
Leonaaard <34171080+Leonaaard@users.noreply.github.com>
2021-10-02 14:36:10 +0200

    scene leo

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+
+    public float speed;
+
+
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
