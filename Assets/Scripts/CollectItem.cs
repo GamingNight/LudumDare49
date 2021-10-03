9daa26d8190aaa4e8eb3954337a5faef55c4e49f
Leonaaard <34171080+Leonaaard@users.noreply.github.com>
2021-10-03 23:14:13 +0200

    stuff

--- Assets/Scripts/CollectItem.cs
+++ Assets/Scripts/CollectItem.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class CollectItem : MonoBehaviour
 {
+
+    private Animator AnimatorController;
+
+
+    void Start() {
+        AnimatorController = gameObject.GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Towerable") {
             CollectableManager.GetInstance().CollectItem(gameObject);
+        }else if (other.gameObject.tag == "Ghost") {
+            AnimatorController.SetBool("Inside", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Ghost") {
+            AnimatorController.SetBool("Inside", false);
         }
     }
 }
