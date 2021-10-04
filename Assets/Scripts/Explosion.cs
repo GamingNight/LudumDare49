d288d67fdbdd7fb68364171bbdb67d5520633f8e
Leonaaard <34171080+Leonaaard@users.noreply.github.com>
2021-10-04 11:40:00 +0200

    grenade 2

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -7,25 +7,35 @@ public class Explosion : MonoBehaviour
     public float radius;
     public float power;
 
+    private AudioSource audioData;
+
     private float timer;
+    private bool exploded;
 
     void Start()
     {
         timer = 0;
+        audioData = GetComponent<AudioSource>();
+        exploded = false;
     }
 
     void Update()
     {
         timer += Time.deltaTime;
 
-        if (timer > 1)
+        if (timer > 1 && !exploded)
         {
             Boom();
+            exploded = true;
         }
     }
 
     void Boom()
     {
+
+        audioData.Play();
+
+        //Physic
         Vector3 explosionPos = transform.position;
         Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
         foreach (Collider hit in colliders)
@@ -35,5 +45,6 @@ public class Explosion : MonoBehaviour
             if (rb != null)
                 rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
         }
+
     }
 }
