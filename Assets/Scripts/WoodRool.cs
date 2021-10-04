2d1b1cf561bbff35cc256eb3a8d085c57ed224a9
Leonaaard <34171080+Leonaaard@users.noreply.github.com>
2021-10-04 17:14:11 +0200

    sounds

---
+++ Assets/Scripts/WoodRool.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodRool : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+    private float initSmallImpactPitch;
+    private float initSmallImpactVolume;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        initSmallImpactPitch = audioSource.pitch;
+        initSmallImpactVolume = audioSource.volume;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        {
+            audioSource.pitch = initSmallImpactPitch + (Random.Range(-1f, 1f));
+            audioSource.volume = initSmallImpactVolume + (0.3f * Random.Range(-1f, 1f));
+            audioSource.Play();
+        }
+    }
+
+}
